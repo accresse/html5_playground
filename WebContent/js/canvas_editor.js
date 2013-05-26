b549397f32156b8821225d0a6603bd748c21e14e
Adam Cresse <adam@cresse.org>
2013-05-25 22:35:27 -0500

    font stuff

--- WebContent/js/canvas_editor.js
+++ WebContent/js/canvas_editor.js
@@ -1,8 +1,10 @@
 var canvas;
 var ctx;
-var bgcolor = "#ffffff";
+var bg_color = "#555555";
+var font_color = "#ffffff";
 var img;
 var heart;
+var canvas_width = 800;
 
 $(document).ready(function() {
 	
@@ -11,6 +13,7 @@ $(document).ready(function() {
 	img = document.createElement("img");
 	heart = document.createElement("img");
 	heart.src='images/heart.png';
+	repaint();
 	
 	$("#canvas_image").change(function(e) {
 	    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
@@ -25,17 +28,43 @@ $(document).ready(function() {
 	});
 	
 	$("#canvas_color").spectrum({
-	    color: bgcolor,
+	    color: bg_color,
     	change: function(color) {
-    		bgcolor = color.toHexString();
+    		bg_color = color.toHexString();
     		repaint();
     	}
 	});
+
+	$("#canvas_font").change(function(e) {
+		repaint();
+	});
+	
+	$("#canvas_font_size").change(function(e) {
+		repaint();
+	});
+	
+	$("#canvas_font_color").spectrum({
+		color: font_color,
+		change: function(color) {
+			font_color = color.toHexString();
+			repaint();
+		}
+	});
+	
+	$("#canvas_text").change(function(e) {
+		repaint();
+	});
+	
 });
 
 function repaint() {
-	ctx.fillStyle = bgcolor;
-	ctx.fillRect(0, 0, 800, 800);
+	ctx.fillStyle = bg_color;
+	ctx.fillRect(0, 0, canvas_width, canvas_width);
     ctx.drawImage(img,0,0);
     ctx.drawImage(heart,0,0);
+    ctx.fillStyle = font_color;
+    ctx.font=$("#canvas_font_size").val()+"px "+$("#canvas_font").val();
+    var text= $("#canvas_text").val();
+    var text_width = ctx.measureText(text).width;
+    ctx.fillText(text,canvas_width/2-text_width/2,canvas_width/2);
 }
